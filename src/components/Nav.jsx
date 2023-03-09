6382da9ec1e01ca070e269bae7b8f819494556be
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-09 08:06:49 +0100

    added the tags

---
+++ src/components/Nav.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { navLinks } from './Data'
+import {FaTimes} from 'react-icons/fa'
+import {CgMenuGridO} from 'react-icons/cg'
+
+const Nav = () => {
+  return (
+    <>
+        <header>
+            <nav>
+                <ul>
+                    {navLinks.map(item => {
+                        const {id, url, link} = item
+                        return <li key={id}><a href={url}>{link}</a></li>
+                    })}
+                </ul>
+                <div>
+                    <CgMenuGridO/>
+                </div>
+            </nav>
+            <div>
+                <div>
+                    <FaTimes/>
+                    <ul>
+                    {navLinks.map(item => {
+                        const {id, url, link} = item
+                        return <li key={id}><a href={url}>{link}</a></li>
+                    })}
+                    </ul>
+
+
+                </div>
+            </div>
+        </header>
+    </>
+  )
+}
+
+export default Nav
