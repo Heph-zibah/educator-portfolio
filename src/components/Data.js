612c8f73b21e89d9d047782e4fac1a1c893181ae
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-09 08:06:16 +0100

    added data file to folder

---
+++ src/components/Data.js
@@ -0,0 +1,27 @@
+export const navLinks = [
+    {
+        id: 1,
+        url: '/#home',
+        link: 'home'
+    },
+    {
+        id: 2,
+        url: '/#about',
+        link: 'about'
+    },
+    {
+        id: 1,
+        url: '/#services',
+        link: 'services'
+    },
+    {
+        id: 1,
+        url: '/#testionies',
+        link: 'testimonies'
+    },
+    {
+        id: 3,
+        url: '/#contact',
+        link: 'contact'
+    },
+]
