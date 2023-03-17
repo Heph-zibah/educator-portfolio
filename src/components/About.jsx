b2bc06f6651394a357a708a2ab8e98763af978c4
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-17 23:28:11 +0100

    update file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,6 +10,13 @@ const About = () => {
             </div>
             <div className="about__content">
                 <h1>about me</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur mollitia tempora, dolorem voluptas possimus c
+                    onsequatur earum, fuga officia dolore rem, illo enim aperiam est dolorum! Libero ex sunt, eveniet non obcaecati 
+                    beatae et veritatis, est cum amet fugiat aperiam temporibus distinctio. Ipsa quibusdam reprehenderit laudantium 
+                    fugit corrupti, dicta harum ratione sed itaque quis veniam? Alias et, 
+                    quasi optio quaerat delectus odio. Asperiores, dolorum ratione praesentium tempora deserunt harum nihil temporibus!
+                </p>
+                <button>Message me</button>
             </div>
         </div>
     </>
