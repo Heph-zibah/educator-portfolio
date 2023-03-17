eca88222d97431fd0eb3c32c717659491d2cbdfa
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-17 22:09:44 +0100

    added home file path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Home from "./components/Home";
 import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div >
+    <div className="container">
       <Nav/>
-      <main className="container">
-
+      <main >
+<Home/>
       </main>
     </div>
   );
