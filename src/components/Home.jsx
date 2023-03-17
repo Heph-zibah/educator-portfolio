c3920f843fea8c6650b806bac370038a68c7ccb8
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-17 22:56:31 +0100

    added the markup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import heroBG from '../assets/hero.png'
+import heroBG from '../assets/herobg.png'
 
 const Home = () => {
   return (
     <>
-        <div><img src={heroBG} alt="" /></div>
+      <div className="hero__container">
+        <div className='hero__img'>
+          <img src={heroBG} alt="illustration" />
+        </div>
+        <div className="hero__content">
+          <h6>Lorem ipsum dolor sit amet consectetur.</h6>
+          <h1>Lorem, ipsum dolor sit amet consectetur adipisicing.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio optio illum deserunt architecto vero veniam commodi
+             id unde laboriosam expedita! Tempore delectus sunt corporis reiciendis cum facere, incidunt vitae deserunt.
+          </p>
+          <button>Message me</button>
+        </div>
+      </div>
     </>
   )
 }
