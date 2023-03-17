6af62a2158ffcd551e35d4e1f3e7d7ebe4124296
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-17 22:10:03 +0100

    added the image

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import heroBG from '../assets/hero.png'
+
+const Home = () => {
+  return (
+    <>
+        <div><img src={heroBG} alt="" /></div>
+    </>
+  )
+}
+
+export default Home
