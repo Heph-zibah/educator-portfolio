e07eceb6dace67a1359ed9457fad8bc6062f8ef0
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-18 13:50:24 +0100

    added the markup

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 const Contact = () => {
   return (
     <>
-        <div>
+        <div className='contact__container'>
             <h1>Get In Touch</h1>
-            <form action="">
-                
+            <form className='contact__form'>
+                <input type="text"  placeholder='Your Name'/>
+                <input type="email" placeholder='Your Email' />
+                <input type="number" placeholder='Your Whatsapp Number' />
+                <textarea name="message" id="" cols="50" rows="10">Message</textarea>
+                <button>Send</button>
             </form>
         </div>
 
