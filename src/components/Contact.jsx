2f7999fa73e1778a1f32ad3e39c2ca06900aff63
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-18 14:08:50 +0100

    added a paragraph

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,11 +5,12 @@ const Contact = () => {
     <>
         <div className='contact__container'>
             <h1>Get In Touch</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, in.</p>
             <form className='contact__form'>
                 <input type="text"  placeholder='Your Name'/>
                 <input type="email" placeholder='Your Email' />
-                <input type="number" placeholder='Your Whatsapp Number' />
-                <textarea name="message" id="" cols="50" rows="10">Message</textarea>
+                <input type="numeric" placeholder='Your Whatsapp Number' />
+                <textarea name="message" id="" cols="50" rows="10" value='Message'></textarea>
                 <button>Send</button>
             </form>
         </div>
