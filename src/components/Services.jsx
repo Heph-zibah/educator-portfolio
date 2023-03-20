4a97ce5c5fcd94fdfd9711a0b55447315aec7791
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-20 21:25:05 +0100

    added the markup

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import {GiArchiveResearch} from 'react-icons/gi'
+import {FaChalkboardTeacher} from 'react-icons/fa'
+import {TfiWrite} from 'react-icons/tfi'
 
 const Services = () => {
   return (
-    <div>Services</div>
+   <>
+    <div className='services__container'>
+        <h1>services</h1>
+        <div className="services__cards">
+            <div className="services__card">
+                <div className='services__card--head'>
+                    <FaChalkboardTeacher/>
+                    <h6>Teaching</h6>
+                </div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate provident voluptatem, 
+                    iusto voluptatibus autem, amet aut maxime ex doloremque quaerat quia quisquam, ducimus 
+                    eos minima delectus atque? Aut, corporis pariatur?
+                </p>
+            </div>
+            <div className="services__card">
+                <div className='services__card--head'>
+                    <TfiWrite/>
+                    <h6>Writing</h6>
+                </div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate provident voluptatem, 
+                    iusto voluptatibus autem, amet aut maxime ex doloremque quaerat quia quisquam, ducimus 
+                    eos minima delectus atque? Aut, corporis pariatur?
+                </p>
+            </div>
+            <div className="services__card">
+                <div className='services__card--head'>
+                    <GiArchiveResearch/>
+                    <h6>Research</h6>
+                </div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate provident voluptatem, 
+                    iusto voluptatibus autem, amet aut maxime ex doloremque quaerat quia quisquam, ducimus 
+                    eos minima delectus atque? Aut, corporis pariatur?
+                </p>
+            </div>
+        </div>
+        
+    </div>
+   </>
   )
 }
 
-export default Services
+export default Services 
