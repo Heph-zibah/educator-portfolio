641f73c8bb9ae27f7a54622b028ea8b527db6eba
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-20 21:02:13 +0100

    added services component file path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main >
         <Home/>
         <About/>
+        <Services/>
         <Contact/>
       </main>
     </div>
