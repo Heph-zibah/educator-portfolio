7a0c7710e1ca19b5ce950690e76b421cf50a514f
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-21 07:54:09 +0100

    moved the file to component folder

---
+++ src/components/Testimonies.jsx
@@ -0,0 +1,59 @@
+import React , {useState} from 'react';
+import { reviewData } from './Testimonies/data';
+import {MdArrowBackIosNew, MdArrowForwardIos} from 'react-icons/md'
+import {BsFillChatSquareQuoteFill} from 'react-icons/bs'
+
+const Testimonies = () => {
+  const [index, setIndex] = useState(0)
+  const {id, text, name, job} = reviewData[index]
+  const checkNumber = (number) => {
+    if (number > reviewData.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return reviewData.length - 1;
+    }
+    return number;
+  }
+
+  const prev = () => {
+    setIndex((index) => {
+      let newIndex = index -1;
+      return checkNumber(newIndex);
+    })
+  }
+
+  const next = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex)
+    })
+  }
+
+  return (
+    <>
+    <div className="testimonies__container">
+      <h1>Testimonies</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, ipsum.</p>
+      <div className="testimonies__cards">
+        <div className="testimonies__card" key={id}>
+          <BsFillChatSquareQuoteFill className='testimonies__quote'/>
+          <p>{text}</p>
+          <strong>{name}</strong>
+          <small>{job}</small>
+          <div className='testimonies__icons'>
+            <div onClick={prev}>
+              <MdArrowBackIosNew/>
+            </div>
+            <div onClick={next}>
+              <MdArrowForwardIos/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Testimonies
