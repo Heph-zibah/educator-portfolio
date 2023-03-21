1cc4640ff4b56a2e58971fbf4e5c7dbe13c70115
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-21 06:45:56 +0100

    update the data file

---
+++ src/components/Testimonies/data.js
@@ -0,0 +1,32 @@
+export const reviewData  = [
+    {
+        id:1,
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate provident voluptatem, iusto voluptatibus autem, amet aut maxime ex doloremque quaerat quia quisquam, ducimus eos minima delectus atque? Aut, corporis pariatur?',
+        name: 'Andery Noah',
+        job: 'nurse'
+    },
+    {
+        id:2,
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate provident voluptatem, iusto voluptatibus autem, amet aut maxime ex doloremque quaerat quia quisquam, ducimus eos minima delectus atque? Aut, corporis pariatur?',
+        name: 'Levi Sandra',
+        job: 'Doctor'
+    },
+    {
+        id:3,
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate provident voluptatem, iusto voluptatibus autem, amet aut maxime ex doloremque quaerat quia quisquam, ducimus eos minima delectus atque? Aut, corporis pariatur?',
+        name: 'Yvonne Daniel',
+        job: 'Entrepreneur'
+    },
+    {
+        id:4,
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate provident voluptatem, iusto voluptatibus autem, amet aut maxime ex doloremque quaerat quia quisquam, ducimus eos minima delectus atque? Aut, corporis pariatur?',
+        name: 'Andery Noah',
+        job: 'Medical Practitioner'
+    },
+    {
+        id:5,
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate provident voluptatem, iusto voluptatibus autem, amet aut maxime ex doloremque quaerat quia quisquam, ducimus eos minima delectus atque? Aut, corporis pariatur?',
+        name: 'Fernadez Joan',
+        job: 'Student'
+    },
+]
