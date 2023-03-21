a83bd833e8c416852bffc6c31aef2243a434633d
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-21 08:12:35 +0100

    update file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { navLinks } from './Data'
 
 const footer = () => {
   return (
     <>
         <footer>
             <h1>Lucid Nelson</h1>
+            <ul>
+                {navLinks.map(item => {
+                    const {id, url, link} = item
+                    return <li key={id}><a href={url}>{link}</a></li>
+                })}
+            </ul>
+            <p>All Rights Reserved</p>
         </footer>
     </>
   )
