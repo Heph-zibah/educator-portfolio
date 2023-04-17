25bd38f1ceec9b46fc5dc1fa80b06688c5693aa3
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-04-17 15:02:02 +0100

    update file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,20 +4,36 @@ import aboutBg from '../assets/aboutbg.png'
 const About = () => {
   return (
     <>
-        <div className='about__container' name='about'>
-            <div className="about__img">
-                <img src={aboutBg} alt="" />
-            </div>
-            <div className="about__content">
-                <h1>about me</h1>
-                <p>Seeing my students excel is one of the most fulfilling aspects of my profession. Witnessing their progress, growth, and achievements brings me immense joy and satisfaction. 
-                    I am dedicated to equipping my students with the skills, knowledge, and confidence they need to thrive in their academic pursuits and beyond.</p>
-                <p>With a strong team spirit, I actively collaborate with colleagues, students, and parents to foster a supportive and engaging learning community. I am committed to understanding the unique strengths and weaknesses of my students and providing personalized guidance to help them excel academically and personally. Witnessing my students' progress and achievements brings me immense joy, and I am dedicated to equipping them with the skills, knowledge, and confidence they need to succeed in their language learning journey and beyond.</p>
-                <button>Message me</button>
-            </div>
+      <div className="about__container" name="about">
+        <div className="about__img">
+          <img src={aboutBg} alt="" />
         </div>
+        <div className="about__content">
+          <h1>about me</h1>
+          <p>
+            Seeing my students excel is one of the most fulfilling aspects of my
+            profession. Witnessing their progress, growth, and achievements
+            brings me immense joy and satisfaction. I am dedicated to equipping
+            my students with the skills, knowledge, and confidence they need to
+            thrive in their academic pursuits and beyond.
+          </p>
+          <p>
+            With a strong team spirit, I actively collaborate with colleagues,
+            students, and parents to foster a supportive and engaging learning
+            community. I am committed to understanding the unique strengths and
+            weaknesses of my students and providing personalized guidance to
+            help them excel academically and personally. Witnessing my students'
+            progress and achievements brings me immense joy, and I am dedicated
+            to equipping them with the skills, knowledge, and confidence they
+            need to succeed in their language learning journey and beyond.
+          </p>
+          <a href="https://wa.link/pvxoki" target="_blank">
+            Message me
+          </a>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default About
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,22 +4,28 @@ import heroBG from '../assets/herobg.png'
 const Home = () => {
   return (
     <>
-      <div className="hero__container" name='home'>
-        <div className='hero__img'>
+      <div className="hero__container" name="home">
+        <div className="hero__img">
           <img src={heroBG} alt="illustration" />
         </div>
         <div className="hero__content">
           <h6>Passionate about Teaching, Research, and Student Success</h6>
           <h1>Educator, Researcher, IELTS Expert</h1>
-          <p>I am Daramola Nelson, an experienced educator, researcher, and IELTS expert. With a deep passion for education,
-             I am dedicated to empowering students to achieve their language learning goals. I bring more than seven years of experience in teaching
-             English and expertise in language research to create dynamic and engaging lessons that foster a positive and inclusive learning environment.
+          <p>
+            I am Daramola Nelson, an experienced educator, researcher, and IELTS
+            expert. With a deep passion for education, I am dedicated to
+            empowering students to achieve their language learning goals. I
+            bring more than seven years of experience in teaching English and
+            expertise in language research to create dynamic and engaging
+            lessons that foster a positive and inclusive learning environment.
           </p>
-          <button>Message me</button>
+          <a href="https://wa.link/pvxoki" target="_blank">
+            Message me
+          </a>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Home
